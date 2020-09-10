341545a07c473344523020c838a0dd9a2a868220
Luis Rivero <luisda021298@gmail.com>
2020-09-10 18:14:11 -0500

    create header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#f3c614',
+        'dark-primary': '#353535',
+        'grey-primary': '#6e6e6e',
+        'secondary': '#2dced6' 
+      }
+    },
   },
   variants: {},
   plugins: [],
